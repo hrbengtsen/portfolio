dffc1f0c97af43bdd47a09be78637cc6e691dbd1
hrbengtsen <mikkelbengtsen@gmail.com>
2019-04-30 11:34:15 +0200

    SPA fix for github pages

--- src/components/main/projects/Portfolio.js
+++ src/components/main/projects/Portfolio.js
@@ -17,7 +17,7 @@ const Portfolio = () => (
       <title>Project: Portfolio - Mikkel Bengtsen</title>
     </Helmet>
     <div className="content padding-as-row anim-seq">
-      <ProjectOverview project={"portfolio"} date={"Mar. 2019"} />
+      <ProjectOverview project={"portfolio"} date={"Apr. 2019"} />
       <ProjectTools tools={[
         "React"
       ]} />
@@ -34,7 +34,7 @@ const Portfolio = () => (
       />
       <Text parentClasses={"section-top"} text={
         <div>
-          <p>The website's concept was to design a bold, professional and minimalistic design, along with a distinct colour palette and overall look.
+          <p>The website's concept was to create a bold, professional and minimalistic design, along with a distinct colour palette and overall look.
           </p>
           <p>In the making of my website I learned the ins and outs of React, which I chose to use, to be able to effortlessly expand the site in any way I want in the future.
           This idea was enforced by following the React workflow of structuring the project in small functional reuseable components.
