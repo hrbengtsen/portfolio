d36b02d241538f282b9836da4f3d634463c08259
hrbengtsen <mikkelbengtsen@gmail.com>
2019-04-30 16:48:46 +0200

    Updates

---
+++ precache-manifest.eb0ff49e8aba6e2848f1357fee266eab.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "4684e3f044b7f61d5fad5b9775fb99dd",
+    "url": "/static/media/kvizboks-video-thumbnail.4684e3f0.jpg"
+  },
+  {
+    "revision": "bab38b21d2760897370a",
+    "url": "/static/css/main.0302d39a.chunk.css"
+  },
+  {
+    "revision": "58b1966bbb1b74bb5a03",
+    "url": "/static/js/1.58b1966b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1493484ca9302d3be99cefbfd405b52b",
+    "url": "/static/media/logo.1493484c.svg"
+  },
+  {
+    "revision": "fa2f7ac7654815a37dbb7120809326df",
+    "url": "/static/media/hero-phone.fa2f7ac7.svg"
+  },
+  {
+    "revision": "7854eb02c361c7dce8475df6fe71f47c",
+    "url": "/static/media/hero-tablet.7854eb02.svg"
+  },
+  {
+    "revision": "a35bfdc207aabc0f3dc334dc0483dbd4",
+    "url": "/static/media/design.a35bfdc2.svg"
+  },
+  {
+    "revision": "f1f22789158899ef700d6d8e3740b2ab",
+    "url": "/static/media/develop.f1f22789.svg"
+  },
+  {
+    "revision": "594ef3b459e0d8cc7bbd8d6d36379c05",
+    "url": "/static/media/kvizboks-objects.594ef3b4.JPG"
+  },
+  {
+    "revision": "e5b827131c5beb0cf47c66f3eb601433",
+    "url": "/static/media/kvizboks-console.e5b82713.jpg"
+  },
+  {
+    "revision": "71420178ccd2f6c46fdb316c46c89a05",
+    "url": "/static/media/kvizboks-controller.71420178.JPG"
+  },
+  {
+    "revision": "8f141474b38508321c73a35e2e95b9f8",
+    "url": "/static/media/kvizboks-game.8f141474.JPG"
+  },
+  {
+    "revision": "14a00987fc70ef51fbb7216a29b6df30",
+    "url": "/static/media/kvizboks-video.14a00987.mp4"
+  },
+  {
+    "revision": "bab38b21d2760897370a",
+    "url": "/static/js/main.bab38b21.chunk.js"
+  },
+  {
+    "revision": "65d20db9f1bc6255914729fdae164f9c",
+    "url": "/static/media/grostedet9-3drender.65d20db9.jpg"
+  },
+  {
+    "revision": "204092d8580ca9bf42017571139810d8",
+    "url": "/static/media/grostedet9-mobilerender.204092d8.png"
+  },
+  {
+    "revision": "4d41449cd5a5f5808ca6f62e53d949b9",
+    "url": "/static/media/grostedet9-tabletrender.4d41449c.jpg"
+  },
+  {
+    "revision": "a5c0723238b41526fa6ccbcac24c61b7",
+    "url": "/static/media/bengtsen-landing.a5c07232.jpg"
+  },
+  {
+    "revision": "17122a756991114d3f5af1c9f6dbe5ec",
+    "url": "/static/media/bengtsen-mobilerender.17122a75.png"
+  },
+  {
+    "revision": "8c7e84259ebf883c7616947b8a26701f",
+    "url": "/static/media/portfolio-logo.8c7e8425.png"
+  },
+  {
+    "revision": "2a44f5538af3e64cef128cc53ca145d6",
+    "url": "/static/media/portfolio-code.2a44f553.png"
+  },
+  {
+    "revision": "4ba482a0928ef3aa41f6340d3ea5a54b",
+    "url": "/static/media/portfolio-render.4ba482a0.png"
+  },
+  {
+    "revision": "2d1a4a6e072d629948d11158833b0382",
+    "url": "/static/media/hero-pc.2d1a4a6e.svg"
+  },
+  {
+    "revision": "db6f3e551da7ca7b6c8c56fdea1df1d6",
+    "url": "/static/media/kvizboks.db6f3e55.png"
+  },
+  {
+    "revision": "71cf1648f2e9f194624ff2c4fcc12edd",
+    "url": "/static/media/portfolio.71cf1648.png"
+  },
+  {
+    "revision": "9a8072cd8e0f20b97e18236bce80cf80",
+    "url": "/static/media/bengtsen-velvaere.9a8072cd.png"
+  },
+  {
+    "revision": "cfdeac31832385e844c49e6c9c27c623",
+    "url": "/static/media/grostedet9.cfdeac31.png"
+  },
+  {
+    "revision": "4c62181025d421b78b207a0d7a4b485d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fa8b85654f888cdeedd05833c87fda5c.js"
+  "/precache-manifest.eb0ff49e8aba6e2848f1357fee266eab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
