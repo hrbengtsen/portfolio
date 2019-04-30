617f63ef619e570bf9410f2ce0c753d86ae03f3b
hrbengtsen <mikkelbengtsen@gmail.com>
2019-04-30 19:31:04 +0200

    hide hover on work page on medium size

--- src/components/other/ProjectCover.js
+++ src/components/other/ProjectCover.js
@@ -3,13 +3,13 @@ import React from 'react';
 const ProjectCover = (props) => (
   <div className={`anim-fadeInUp display-container ${props.parentClasses}`}>
     <div className={props.hover}></div>
-    <div className="display-bottomleft padding-equal display-hover anim-project-info hide-small">
+    <div className="display-bottomleft padding-equal display-hover anim-project-info hide-small hide-medium">
       <h4>{props.title}</h4>
       <span className="margin-right">{props.type}</span>
       <span>{props.date}</span>
     </div>
 
-    <div className="display-bottomleft padding-equal hide-large hide-medium">
+    <div className="display-bottomleft padding-equal hide-large">
       <h4>{props.title}</h4>
       <span className="margin-right">{props.type}</span>
       <span>{props.date}</span>
